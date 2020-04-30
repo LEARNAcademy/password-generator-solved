c1543b53410540f7ab811d9b652e74b384054b65
Harrison Schaen <hschaen@gmail.com>
2020-04-29 17:34:59 -0700

    Update script.js
    fixed last variable

--- script.js
+++ script.js
@@ -221,7 +221,7 @@ var specialCharacters = [
   }
   
   // Get references to the #generate element
-  let generateBtn = document.querySelector('#generate');
+  var generateBtn = document.querySelector('#generate');
   
   // Write password to the #password input
   const writePassword = () => {
